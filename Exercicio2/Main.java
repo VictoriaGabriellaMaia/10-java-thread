efe1a047f73f2609e36a66cd0228ef3d0805a080
Victoria Maia <victoria.gabms@gmail.com>
2026-09-24 11:26:30 -0300

    Adiciona Main do Exercício 2

---
+++ Exercicio2/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        PerfilUsuario perfil = new PerfilUsuario();
+
+        Thread dispositivoMovel = new Thread(() -> {
+            perfil.atualizarPerfil(
+                    "Estudante de Sistemas de Informação",
+                    "Disponível",
+                    "Dispositivo Móvel"
+            );
+        });
+
+        Thread computador = new Thread(() -> {
+            perfil.atualizarPerfil(
+                    "Estudante e profissional de tecnologia",
+                    "Ocupada",
+                    "Computador"
+            );
+        });
+
+        dispositivoMovel.start();
+        computador.start();
+    }
+}
